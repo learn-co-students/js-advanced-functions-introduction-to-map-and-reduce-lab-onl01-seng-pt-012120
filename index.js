9debc353757038d31ada3b38f65c7a00dab8bafb
kelly_manahan <kelly.a.manahan@gmail.com>
2020-08-14 14:14:26 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,53 @@
-// Your code here
+function mapToNegativize(arr){
+    let value  = []
+    for ( let i=0; i < arr.length; i++) {
+        value.push(-1 * arr[i])
+    }
+    return value
+}
+
+function mapToNoChange(arr) {
+    let value = []
+    for (let i = 0; i < arr.length; i++) {
+        value.push(arr[i])
+    }
+    return value
+}
+
+function mapToDouble(arr) {
+    let value = []
+    for (let i = 0; i < arr.length; i++) {
+        value.push(2 * arr[i])
+    }
+    return value
+}
+
+function mapToSquare(arr){
+    let value = []
+    for (let i = 0; i < arr.length; i++) {
+        value.push(arr[i] * arr[i])
+    }
+    return value 
+}
+
+function reduceToTotal(arr, start = 0) {
+    let total = start 
+    for (let i = 0; i < arr.length; i++) {
+        total = total + arr[i]
+    }
+    return total
+}
+
+function reduceToAllTrue(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        if(!arr[i]) return false
+    }
+    return true
+}
+
+function reduceToAnyTrue(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        if(arr[i]) return true
+    }
+    return false
+}
