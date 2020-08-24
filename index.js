c1969d05b7ddcefb9940c212049c24d90793d8f5
hmcdowell95 <hmcdowell95@github.com>
2020-08-24 16:23:17 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Your code here
+function mapToNegativize(a) {return a.map(n => {return n * -1})}
+
+function mapToNoChange(a) {return a.map(n => {return n})}
+
+function mapToDouble(a) {return a.map(n => {return n * 2})}
+
+function mapToSquare(a) {return a.map(n => {return n * n})}
+
+function reduceToTotal(a, s=0) {
+    let t = s;
+    a.forEach(n => {
+        t = t + n
+    });
+    return t
+}
+
+function reduceToAllTrue(a) {
+    let b = true;
+    a.forEach(n => {
+        if (!n) {b = false}
+    });
+    return b
+}
+
+function reduceToAnyTrue(a) {
+    let b = false;
+    a.forEach(n => {
+        if (n) {b = true}
+    });
+    return b
+}
